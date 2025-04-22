a33cd529412d35b2bdb5d74970e1db4262ea019f
DenEvtifeev <D.o.evtifeev@gmail.com>
2025-04-22 19:18:31 +0200

    edited README.md

--- app/Services/DummyService.php
+++ app/Services/DummyService.php
@@ -3,19 +3,24 @@
 namespace App\Services;
 
 use App\Models\Product;
+use Exception;
 use Illuminate\Support\Facades\Http;
 
 class DummyService
 {
     private string $baseUrl = 'https://dummyjson.com/';
     private string $endpoint;
-    private string $filter = '/search?q=';
+    private ?string $filter = null;
     public function __construct(string $endpoint, string $filter){
         $this->endpoint = $endpoint;
-        $this->filter .= $filter;
+        $this->filter = $filter;
     }
     public function getProductData(){
-        $fullUrl = $this->baseUrl.$this->endpoint.$this->filter;
+        if($this->filter !== null){
+            $fullUrl = $this->baseUrl.$this->endpoint.'?q='.$this->filter;
+        }else{
+            $fullUrl = $this->baseUrl.$this->endpoint;
+        }
         $response = HTTP::withoutVerifying()->get($fullUrl)->json();
         $dataForSave = [];
         foreach($response['products'] as $item){
@@ -25,6 +30,12 @@ class DummyService
                 'price' => $item['price'],
             ];
         }
-        Product::upsert($dataForSave, 'product_id');
+        try{
+            Product::upsert($dataForSave, 'product_id');
+            dump('data inserted ' . count($dataForSave));
+        }catch (Exception $e){
+            dump($e->getMessage());
+        }
+
     }
 }
